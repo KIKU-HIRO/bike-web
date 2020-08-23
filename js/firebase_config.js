f7d8bfa13126975cfd07d98bca38d5ff287b55de
KIKU-HIRO <krout0556@gmail.com>
2020-08-23 12:02:12 +0900

    first commit

---
+++ js/firebase_config.js
@@ -0,0 +1,13 @@
+// このファイルは、Firebaseからダウンロードした設定ファイルに差し替えてください
+var firebaseConfig = {
+  apiKey: "XXX",
+  authDomain: "xxx",
+  databaseURL: "https://xxx.firebaseio.com",
+  projectId: "xxx",
+  storageBucket: "xxx.appspot.com",
+  messagingSenderId: "xxxxxxxxx",
+  appId: "1:xxxxxxxx:web:xxxxxxxxxxxxxx",
+  measurementId: "X-XXXXXXXXXXXXXXX"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
